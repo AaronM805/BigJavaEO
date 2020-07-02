5eaa6ab8771944a1436704c92e75e29b638a1461
Aaron Martinez <aaronn.martinez@gmail.com>
2020-07-02 13:42:52 -0700

    Implemented solutions for E15_1, and E15_2. Updated stubs for the rest.

--- src/ch15_the_java_collections_framework/practice_exercises/E15_5.java
+++ src/ch15_the_java_collections_framework/practice_exercises/E15_5.java
@@ -3,7 +3,7 @@ package ch15_the_java_collections_framework.practice_exercises;
 /**
  * Write a program that reads a Java source file and produces an index of all identifiers in the file. For each identifier, print
  * all lines in which it occurs. For simplicity, we will consider each string consisting only of letters, numbers, and
- * underscores an identi- fer. Declare a Scanner in for reading from the source file and call in.useDelimiter("[^A- Za-z0-9_]+").
+ * underscores an identifer. Declare a Scanner in for reading from the source file and call in.useDelimiter("[^A- Za-z0-9_]+").
  * Then each call to next returns an identifier.
  *
  * Difficulty: Hard
