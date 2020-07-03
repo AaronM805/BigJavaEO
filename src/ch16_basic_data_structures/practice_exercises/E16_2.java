ec38912a18236629653e78584fbcd1ad5e8244c9
Aaron Martinez <aaronn.martinez@gmail.com>
2020-07-03 09:26:22 -0700

    Commiting minor changes.

--- src/ch16_basic_data_structures/practice_exercises/E16_2.java
+++ src/ch16_basic_data_structures/practice_exercises/E16_2.java
@@ -1,6 +1,7 @@
 package ch16_basic_data_structures.practice_exercises;
 
 import ch16_basic_data_structures.chapter_examples.section1.LinkedList;
+import ch16_basic_data_structures.chapter_examples.section1.LinkedList.Node;
 /**
  * Consider a version of the LinkedList class of Section 16.1 in which the addFirst method has been replaced with the following
  * faulty version:
@@ -14,7 +15,7 @@ import ch16_basic_data_structures.chapter_examples.section1.LinkedList;
  * Develop a program ListTest with a test case that shows the error. That is, the program should print a failure message with
  * this implementation but not with the correct implementation.
  * 
- * TIME:
+ * TIME: 00:06:28
  * Time Complexity:
  * Space Complexity:
  */
@@ -26,5 +27,16 @@ public class E16_2 {
 }
 
 class MyLinkedList extends LinkedList {
+    public MyLinkedList() {
+        super();
+    }
 
+    @Override
+    public void addFirst(Object element) {
+        Node newNode = new Node();
+        Node head = super.getHead();
+        head = newNode;
+        newNode.data = element;
+        newNode.next = head;
+    }
 }
--- src/ch16_basic_data_structures/practice_exercises/E16_8.java
+++ src/ch16_basic_data_structures/practice_exercises/E16_8.java
@@ -1,6 +1,7 @@
 /**
- * Reimplement the LinkedList class of Section 16.1 so that it implements the java.util. LinkedList interface. Hint: Extend the
- * java.util.AbstractList class.
+ * Reimplement the LinkedList class of Section 16.1 so that it implements the java.util.LinkedList interface.
+ *
+ * Hint: Extend the java.util.AbstractList class.
  * 
  * TIME:
  * Time Complexity:
