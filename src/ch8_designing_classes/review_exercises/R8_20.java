84c0cb22d14b118a5d784e7603908f9813ebb105
Aaron Martinez <aaronn.martinez@gmail.com>
2020-10-01 14:18:13 -0700

    stub for review exercise R8.20.

---
+++ src/ch8_designing_classes/review_exercises/R8_20.java
@@ -0,0 +1,19 @@
+package ch8_designing_classes.review_exercises;
+
+import java.awt.Point;
+
+public class R8_20 {
+
+    public static void main(String[] args) {
+        Double d1 = 123.424;
+        Double d2 = 456.897;
+
+        System.out.println("d1:" + pt.getX() + "\ty:" + pt.getY());
+        swap(d1, d2);
+        System.out.println("x:" + pt.getX() + "\ty:" + pt.getY());
+
+    }
+    public static void swap(Point pt) {
+        double 
+    }
+}
